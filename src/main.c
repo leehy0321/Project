e2f85af1fd3631e5cabd7f712abc8155f3a60b7d
Hwayoung Baek <wwwo3owww@naver.com>
2016-11-10 04:41:20 -0800

    second

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-#include <conio.h>  // gotoxy  (1~80,1~24)
-#include <windows.h>
+#include <ncurses.h>  // gotoxy  (1~80,1~24)
+//#include <windows.h>
+
+#define gotoxy(x,y) wmove(stdscr,y-1,x-1)
 
 #define SHAPE_O 0  //'¡Ü'
 #define SHAPE_D 1  //'¡á'
@@ -16,8 +18,10 @@
 ///////////////////////////////////////////////////////////////////////
 //variable
 
-int block[5][2] = { { '¡Û', '¡Ü' },{ '¡à','¡á' },{ '¡â','¡ã' },
-					{ '¢œ','¢Ÿ' },{ '¢»','¢Œ' } };
+//int block[5][2] = { { '¡Û', '¡Ü' },{ '¡à','¡á' },{ '¡â','¡ã' },
+//					{ '¢œ','¢Ÿ' },{ '¢»','¢Œ' } };
+int block[5][2];
+
 // [0][0] : '¡Û'
 // [0][1] : '¡Ü'
 
@@ -26,26 +30,32 @@ int board_cpy[B_SIZE_X][B_SIZE_Y];  // prior board(cpy)
 
 /////////////////////////////////////////////////////////////////////////
 // functions
-void gotoxy(int x, int y);
+//void gotoxy(int x, int y);
 void draw_gameBoard();
 
 
 /////////////////////////////////////////////////////////////////////////
 int main(void)
 {
-	for(int i=0; i<5; i++)
-	{
-		printf("block[%d][0] = %c\n",i,block[i][0]);
-		printf("block[%d][1] = %c\n", i, block[i][1]);
-	}
+	gotoxy(3,4);
+	printf("here is (3,4)\n");
+	
+	gotoxy(10,3);
+	printf("here is (10,3)\n");
+	//for(int i=0; i<5; i++)
+	//{
+	//	printf("block[%d][0] = %c\n",i,block[i][0]);
+	//	printf("block[%d][1] = %c\n", i, block[i][1]);
+	//}
 	return 0;
 }
 
-void gotoxy(int x, int y)
-{
-	COORD Pos = { 2*x,y };  // why multiple 2 **
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),Pos);
-}
+//void gotoxy(int x, int y)
+//{
+	//COORD Pos = { 2*x,y };  // why multiple 2 **
+	//SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),Pos);
+//	printf("%c[%d;%df",0x1B,y,x);
+//}
 
 void draw_gameBoard() // draw to 
 {
